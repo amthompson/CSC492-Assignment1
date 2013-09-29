df2713617f366c530250a45bb2460225636697e9
amthompson <andrew.thompson@mines.sdsmt.edu>
2013-09-29 17:52:57 -0600

    Layout update

--- gen/edu/sdsmt/thompsonsamson/assignment1/R.java
+++ gen/edu/sdsmt/thompsonsamson/assignment1/R.java
@@ -10,11 +10,58 @@ package edu.sdsmt.thompsonsamson.assignment1;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int tagged_search_background=0x7f040000;
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int tagged_search_size=0x7f050002;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int TableLayout1=0x7f090000;
+        public static final int TableLayout2=0x7f09000a;
+        public static final int action_settings=0x7f090010;
+        public static final int button1=0x7f090005;
+        public static final int button2=0x7f09000c;
+        public static final int button_delete=0x7f09000f;
+        public static final int button_edit=0x7f09000e;
+        public static final int button_tag=0x7f09000d;
+        public static final int editText1=0x7f090002;
+        public static final int editText2=0x7f090004;
+        public static final int scrollView1=0x7f090009;
+        public static final int tableRow1=0x7f090001;
+        public static final int tableRow2=0x7f090003;
+        public static final int tableRow3=0x7f090006;
+        public static final int tableRow4=0x7f090008;
+        public static final int tableRow5=0x7f09000b;
+        public static final int textView1=0x7f090007;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+        public static final int tagged_search=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main=0x7f080000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int button_clear=0x7f060007;
+        public static final int button_delete=0x7f060006;
+        public static final int button_edit=0x7f060005;
+        public static final int button_save=0x7f060003;
+        public static final int tagged_search_title=0x7f060004;
+        public static final int title_activity_main=0x7f060001;
     }
     public static final class style {
         /** 
@@ -38,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f070001;
     }
 }
