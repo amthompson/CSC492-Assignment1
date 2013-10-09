61e289f806ab86e9f85acef5b104b717ec3f07fb
amthompson <andrew.thompson@mines.sdsmt.edu>
2013-10-08 23:44:03 -0600

    Commented code. Forgot to upload once power came back on.

--- src/edu/sdsmt/thompsonsamson/assignment1/MainActivity.java
+++ src/edu/sdsmt/thompsonsamson/assignment1/MainActivity.java
@@ -1,5 +1,21 @@
+/******************************************************************************
+ *	Project:	Assignment 1 - Twitter Tag Search
+ *	Authors:	Andrew Thompson & Scott Samson
+ *	Date:		10/1/2013
+ *	Purpose:	Android application to save twitter tags and call searches.
+ *				Items used: SharedPreferences, Intents, Anonymous Inner Classes,
+ *				Inflating views, and layout design.
+ *	Repository:	https://github.com/amthompson/CSC421-Assignment1
+ *	Timeline:	9/28/2013:	Initial layout design (AT)
+ *				10/1/2013:	Program functionality (AT&SS)
+ *				10/2/2013:	Edit layout colors and icon (AT)
+ *				10/3/2013:	Fix layout bugs (AT)
+ *				10/4/2013:	Finalize comments
+ *	Bugs:		none found yet 
+ ******************************************************************************/
 package edu.sdsmt.thompsonsamson.assignment1;
 
+// imported libraries
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Arrays;
@@ -10,24 +26,34 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
 
+
+/******************************************************************************
+ * Class:	MainActivity
+ * Purpose:	Main entry to the application. Create twitter search tags and
+ * 			store them in shared preferences. Allow user to edit and delete
+ * 			tags and to clear all tags.
+ *****************************************************************************/
 public class MainActivity extends Activity {
 
-	SharedPreferences _savedSearches;
-	EditText _objQuery;
-	EditText _objTag;
-	TableLayout _tagListTableLayout;
-	String _editTag;
+	SharedPreferences _savedSearches;		// saved search tags
+	EditText _objQuery;						// query text field
+	EditText _objTag;						// tag text field
+	TableLayout _tagListTableLayout;		// tag search table
+	String _editTag;						// current tag being edited
 	
-	/* Method:	onCreate
-	 * Purpose:	Main entry point for activity. Called when application is created.
-	 */
+	/**************************************************************************
+	 * Method:	onCreate
+	 * Purpose:	Main entry point for activity. Called when application is
+	 * 			created. Defines activity-wide members and click listeners for 
+	 * 			save and clear buttons.
+	 * Params:	savedInstanceState - Bundle to record lifecycle data
+	 **************************************************************************/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -64,9 +90,11 @@ public class MainActivity extends Activity {
 		getTaggedSearches();
 	}
 
-	/* Method:	saveTag
-	 * Purpose:	Adds the query/tag information to the saved preferences and inflates a view
-	 */
+	/**************************************************************************
+	 * Method:	saveTag
+	 * Purpose:	Add or updates the query andtag information to the saved preferences
+	 * Params:	none
+	 **************************************************************************/
 	private void saveTag()
 	{
 		// get text fields values
@@ -80,7 +108,8 @@ public class MainActivity extends Activity {
 			return;
 		}
 		
-		// add the tag to saved preferences
+		// add the tag to saved preferences. if the edit tag member
+		// has a value, then remove before adding the tag
 		SharedPreferences.Editor prefEditor = _savedSearches.edit();
 		if (_editTag != null && _editTag != tag)
 		{
@@ -90,7 +119,7 @@ public class MainActivity extends Activity {
 		prefEditor.putString( tag, query );
 		prefEditor.commit();
 				
-		// clear the text fields
+		// clear the text fields and remove focus
 		_objQuery.setText("");
 		_objTag.setText("");
 		_objQuery.clearFocus();
@@ -100,65 +129,11 @@ public class MainActivity extends Activity {
 		getTaggedSearches();
 	}
 
-	private void sendClearTagsAlert() {
-		// build the alert dialog
-		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-		alertBuilder.setTitle(R.string.alert_clear_title);
-		alertBuilder.setMessage(R.string.alert_clear_message);
-		alertBuilder.setCancelable(true);
-		
-		// listener for confirmation
-		DialogInterface.OnClickListener deleteAllTagsConfirmButtonListener = new DialogInterface.OnClickListener() {						
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				deleteAllTags();
-			}
-		};
-		
-		alertBuilder.setPositiveButton(R.string.alert_yes, deleteAllTagsConfirmButtonListener);
-		alertBuilder.setNegativeButton(R.string.alert_cancel, null);
-
-		// show the alert
-		AlertDialog alert = alertBuilder.create();
-		alert.show();
-	}
-	
-	private void sendDeleteTagAlert(final String tag) {
-		// build the alert dialog
-		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-		alertBuilder.setTitle(R.string.alert_delete_title);
-		alertBuilder.setMessage(R.string.alert_delete_message);
-		alertBuilder.setCancelable(true);
-
-		// listener for confirmation
-		DialogInterface.OnClickListener deleteTagConfirmButtonListener = new DialogInterface.OnClickListener() {						
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				deleteTag(tag);
-			}
-		};		
-
-		alertBuilder.setPositiveButton(R.string.alert_yes, deleteTagConfirmButtonListener);
-		alertBuilder.setNegativeButton(R.string.alert_cancel, null);
-		
-		// show the alert
-		AlertDialog alert = alertBuilder.create();
-		alert.show();		
-	}
-	
-	private void sendCheckInputAlert() {
-		// build the alert dialog
-		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-		alertBuilder.setTitle(R.string.alert_save_title);
-		alertBuilder.setMessage(R.string.alert_save_message);
-		alertBuilder.setCancelable(true);
-		alertBuilder.setNegativeButton(R.string.alert_ok, null);
-		
-		// show the alert
-		AlertDialog alert = alertBuilder.create();
-		alert.show();
-	}
-	
+	/**************************************************************************
+	 * Method: 	getTaggedSearches
+	 * Purpose:	Display the tagged searches from the shared preferences.
+	 * Params:	none
+	 *************************************************************************/
 	private void getTaggedSearches ()
 	{	
 		// clear any existing view
@@ -181,15 +156,16 @@ public class MainActivity extends Activity {
 			final Button tagButton = (Button) tagRowView.findViewById(R.id.button_tag);			
 			tagButton.setText(tag);
 						
+			// create a listener when the tag button is clicked
 			tagButton.setOnClickListener(new Button.OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					displaySearch(tagButton.getText().toString());
+					sendQueryIntent(tagButton.getText().toString());
 				}
 			});
 			
-			// edit button and listener
-			Button editButton = (Button) tagRowView.findViewById(R.id.button_edit);
+			// instantiate edit button and create click listener
+			final Button editButton = (Button) tagRowView.findViewById(R.id.button_edit);
 			editButton.setOnClickListener(new Button.OnClickListener() {
 				@Override
 				public void onClick(View v) {
@@ -197,8 +173,8 @@ public class MainActivity extends Activity {
 				}
 			});	
 			
-			// delete button and listener
-			Button deleteButton = (Button) tagRowView.findViewById(R.id.button_delete);
+			// instantiate delete button and click listener
+			final Button deleteButton = (Button) tagRowView.findViewById(R.id.button_delete);
 			deleteButton.setOnClickListener(new Button.OnClickListener() {
 				@Override
 				public void onClick(View v) {
@@ -211,21 +187,37 @@ public class MainActivity extends Activity {
 		}	
 	}
 
-	public void displaySearch(String tag) {
+	/**************************************************************************
+	 * Method:	sendQueryIntent
+	 * Purpose:	Sends intent when a tag is clicked on. URL encodes the query 
+	 * 			string and appends it onto the search url.
+	 * Params:	tag - the unique tag to lookup the key/pair value
+	 *************************************************************************/
+	public void sendQueryIntent(String tag) {
 
+		// the URL encoder is required to be in a try/catch block
 		try {
-
+			
+			// build the query string
 			String query = _savedSearches.getString(tag, null);
 			String url = getString(R.string.search_url) + URLEncoder.encode(query, "UTF-8");
+			
+			// fire the intent
 			Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 			startActivity (webIntent);
-			Log.v("search",query);
-		
+			
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**************************************************************************
+	 * Method: 	editTag
+	 * Purpose:	Edit an existing search tag. Sets the application variable 
+	 * 			to record which tag is being edited and fills in the query/tag text
+	 * 			fields. The save click listener will update the tag.
+	 * Params:	tag - the unique tag to lookup the key/pair value
+	 *************************************************************************/
 	public void editTag(String tag) {
 		// get query from tag pairing in shared preferences
 		String query = _savedSearches.getString(tag, null);
@@ -236,22 +228,113 @@ public class MainActivity extends Activity {
 		
 		_editTag = tag;
 	}
-	
+
+	/**************************************************************************
+	 * Method:	deleteTag
+	 * Purpose:	Delete an existing tag from the saved preferences.
+	 * Params:	tag - the unique tag to lookup the key/pair value
+	 *************************************************************************/
 	private void deleteTag(String tag) {
 		
+		// create the editor and remove the tag
 		SharedPreferences.Editor prefEditor = _savedSearches.edit();
 		prefEditor.remove(tag);
 		prefEditor.commit();
 		
+		// refresh the current tag view
 		getTaggedSearches();
 	}
 
-	// clear all tags - be sure to do alert
+	/**************************************************************************
+	 * Method:	deleteAllTags
+	 * Purpose: Deletes all key/value pairs from the saved preferences and
+	 * 			removes all the inflated views from the tagged searches.
+	 * Params:	none
+	 *************************************************************************/
 	private void deleteAllTags() {
+
+		// create the editor and remove all tags
 		SharedPreferences.Editor prefEditor = _savedSearches.edit();
 		prefEditor.clear();
 		prefEditor.commit();
 		
+		// remove all inflated tagged search views
 		_tagListTableLayout.removeAllViews();	
+	}
+
+	/**************************************************************************
+	 * Method:	sendCheckInputAlert
+	 * Purpose:	Sends an alert if both the query and tag text fields are empty
+	 * Params:	none
+	 *************************************************************************/
+	private void sendCheckInputAlert() {
+		// build the alert dialog
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+		alertBuilder.setTitle(R.string.alert_save_title);
+		alertBuilder.setMessage(R.string.alert_save_message);
+		alertBuilder.setCancelable(true);
+		alertBuilder.setNegativeButton(R.string.alert_ok, null);
+		
+		// show the alert
+		AlertDialog alert = alertBuilder.create();
+		alert.show();
+	}
+
+	/**************************************************************************
+	 * Method:	sendDeleteTagAlert
+	 * Purpose:	Sends an alert to the user to confirm a single tag delete
+	 * Params:	none
+	 *************************************************************************/
+	private void sendDeleteTagAlert(final String tag) {
+		// build the alert dialog
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+		alertBuilder.setTitle(R.string.alert_delete_title);
+		alertBuilder.setMessage(R.string.alert_delete_message);
+		alertBuilder.setCancelable(true);
+	
+		// listener for confirmation
+		DialogInterface.OnClickListener deleteTagConfirmButtonListener = new DialogInterface.OnClickListener() {						
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				deleteTag(tag);
+			}
+		};		
+	
+		// add the buttons - if yes, fire listener
+		alertBuilder.setPositiveButton(R.string.alert_yes, deleteTagConfirmButtonListener);
+		alertBuilder.setNegativeButton(R.string.alert_cancel, null);
+		
+		// show the alert
+		AlertDialog alert = alertBuilder.create();
+		alert.show();		
+	}
+
+	/**************************************************************************
+	 * Method:	sendClearTagsAlert
+	 * Purpose:	Sends an alert to the user to confirm deletion of all searches
+	 * Params:	none
+	 *************************************************************************/
+	private void sendClearTagsAlert() {
+		// build the alert dialog
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+		alertBuilder.setTitle(R.string.alert_clear_title);
+		alertBuilder.setMessage(R.string.alert_clear_message);
+		alertBuilder.setCancelable(true);
+		
+		// listener for confirmation
+		DialogInterface.OnClickListener deleteAllTagsConfirmButtonListener = new DialogInterface.OnClickListener() {						
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				deleteAllTags();
+			}
+		};
+		
+		// add the buttons - if yes, fire listener
+		alertBuilder.setPositiveButton(R.string.alert_yes, deleteAllTagsConfirmButtonListener);
+		alertBuilder.setNegativeButton(R.string.alert_cancel, null);
+	
+		// show the alert
+		AlertDialog alert = alertBuilder.create();
+		alert.show();
 	}
 }
